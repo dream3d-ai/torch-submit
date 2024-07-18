cd062c4ecc04f0dfe64182b076e51d374b9f2cc2
Tony Francis <tony@dream3d.com>
2024-07-18 17:10:05 -0400

    add --version

--- torch_submit/cli.py
+++ torch_submit/cli.py
@@ -1,3 +1,5 @@
+from importlib.metadata import version
+
 import typer
 
 from .commands import cluster, job
@@ -6,6 +8,26 @@ app = typer.Typer()
 
 app.add_typer(cluster.app, name="cluster")
 app.add_typer(job.app, name="job")
+
+
+def version_callback(value: bool):
+    if value:
+        print(f"torch-submit version: {version('torch-submit')}")
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    version: bool = typer.Option(
+        None,
+        "--version",
+        callback=version_callback,
+        is_eager=True,
+        help="Show the version and exit.",
+    ),
+):
+    pass
+
 
 if __name__ == "__main__":
     app()
