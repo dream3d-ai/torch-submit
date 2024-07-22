475c08521ad4d612f53ec9cc43167c6e960c94ca
Tony Francis <tony@dream3d.com>
2024-07-22 13:49:26 -0400

    submit paramiko warning

--- torch_submit/__init__.py
+++ torch_submit/__init__.py
@@ -1,5 +1,10 @@
+import warnings
+
 from . import utils
 from ._version import version as __version__
 from .cli import app as cli
+
+warnings.filterwarnings(action="ignore", module=".*paramiko.*")
+
 
 __all__ = ["cli", "utils", "__version__"]
