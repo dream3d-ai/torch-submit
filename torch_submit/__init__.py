206c37650e971ce1aa1caf57ee724d719b8f36dc
VikaasVarma <vikaas.varma@gmail.com>
2024-07-23 13:17:09 -0400

    Remove warning

--- torch_submit/__init__.py
+++ torch_submit/__init__.py
@@ -1,10 +1,11 @@
 import warnings
 
+warnings.filterwarnings(action="ignore", module=".*paramiko.*")
+
 from . import utils
 from ._version import version as __version__
 from .cli import app as cli
 
-warnings.filterwarnings(action="ignore", module=".*paramiko.*")
 
 
 __all__ = ["cli", "utils", "__version__"]
